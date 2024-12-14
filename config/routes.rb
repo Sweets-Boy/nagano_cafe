50013de3c41274810cd0f1dbf16147611c8605d2
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2024-12-14 02:22:08 +0000

    migrate

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/homes/about' => 'homes#about', as: 'about'
   devise_for :users
   post '/search',  to: 'items#search'
-  get  '/search',  to: 'items#search'
+  get '/search', to: 'items#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
  
