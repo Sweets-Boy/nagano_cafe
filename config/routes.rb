559c1b6ccd2c6760548a9c192df37cd5bc4cd215
EC2 Default User <ec2-user@ip-172-31-46-192.eu-north-1.compute.internal>
2024-12-14 02:55:05 +0000

    [Add]controller, ルーティング設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :order_details, only: [:update]
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :items, only: [:index, :new, :show, :edit, :create, :update]
+    get 'homes/top'
+  end
+
+
+  namespace :public do
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:index, :new, :show, :create]
+    post 'orders/confirm'
+    get 'orders/thanks'
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    delete 'destroy_all'
+    resources :customers, only: [:show, :edit, :update]
+    patch 'customers/withdrawal'
+    get 'customers/unsubscribe'
+    resources :items, only: [:index, :show]
+  end
+
+  root to: 'homes#top'
   get '/homes/top' => 'homes#top', as: 'top'
   get '/homes/about' => 'homes#about', as: 'about'
   devise_for :users
