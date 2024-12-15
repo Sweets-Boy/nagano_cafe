0f6ffe9d941fd2517cac50a7c7baa9080aa3ebf7
EC2 Default User <ec2-user@ip-172-31-46-192.eu-north-1.compute.internal>
2024-12-15 07:31:22 +0000

    商品新規登録フォーム作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,10 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :genre_id, presence: true
   validates :name, presence: true
-  validates :introduction, presence: true, length: { maxmun:150 }
+  validates :introduction, presence: true, length: { maximum:150 }
   validates :price, presence:true
+  validates :is_active, inclusion: {in: [true, false]}
 
 end
