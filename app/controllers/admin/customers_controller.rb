1f14706f5a61de1406e6d3246cb28ba5e52ade92
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2024-12-16 17:00:49 +0000

    customer_controller

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,4 +11,12 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: '顧客情報が更新されました。'
+    else
+      render :edit
+    end
 end
