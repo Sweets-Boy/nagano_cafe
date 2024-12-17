9eab7dc12bc6be92fec7e0145f888f5733138938
EC2 Default User <ec2-user@ip-172-31-37-105.eu-north-1.compute.internal>
2024-12-17 01:54:23 +0000

    route編集

--- config/routes.rb
+++ config/routes.rb
@@ -25,15 +25,16 @@ devise_for :customers,skip: [:passwords], controllers: {
   namespace :public do
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
     resources :orders, only: [:index, :new, :show, :create]
-    post 'orders/confirm'
-    get 'orders/thanks'
-
+      collection do
+        post 'orders/confirm'
+        get 'orders/thanks'
+      end
+    end
     resources :cart_items, only: [:index, :create, :update, :destroy] do
       collection do
         delete 'destroy_all'
       end
     end
-    
     resources :customers, only: [:show, :edit, :update]
     patch 'customers/withdrawal'
     get 'customers/unsubscribe'
