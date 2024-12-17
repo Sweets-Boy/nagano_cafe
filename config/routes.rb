1185341a62973ac78939fc5fcca620ceb3a5478a
EC2 Default User <ec2-user@ip-172-31-37-105.eu-north-1.compute.internal>
2024-12-17 02:41:40 +0000

    config 編集

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ devise_for :customers,skip: [:passwords], controllers: {
 
   namespace :public do
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    resources :orders, only: [:index, :new, :show, :create]
+    resources :orders, only: [:index, :new, :show, :create] do
       collection do
         post 'orders/confirm'
         get 'orders/thanks'
