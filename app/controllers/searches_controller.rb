22ae6b3b31d9d27aaca256f1b992bad15e0f6809
EC2 Default User <ec2-user@ip-172-31-46-192.eu-north-1.compute.internal>
2024-12-18 06:33:04 +0000

    ジャンル検索実装完了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,6 +10,8 @@ class SearchesController < ApplicationController
 
   def genre_search
     @genre_id = params[:genre_id]
+    @genre = Genre.find(@genre_id)
+    @genres = Genre.all
     @items = Item.where(genre_id: @genre_id).page(params[:page]).per(8)
   end
 
