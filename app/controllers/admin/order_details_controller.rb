544b14d7345ff7836c21f2ccff3857f47d6a335e
EC2 Default User <ec2-user@ip-172-31-37-105.eu-north-1.compute.internal>
2024-12-19 12:50:52 +0000

    admin order status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,6 +3,14 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
+    if @order_detail.update(order_detail_params)
+      if @order_detail.making_status == "製作中"
+        @order.update(status: :"製作中")
+      elsif @order.order_details.all? { |order_detail| order_detail.making_status == "製作完了" }
+        @order.update(status: :"発送準備中")
+      end
+    end
+    redirect_to admin_order_path(@order)
   end
 
   private
@@ -10,5 +18,4 @@ class Admin::OrderDetailsController < ApplicationController
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
   end
-
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,4 +5,9 @@ class OrderDetail < ApplicationRecord
 
   enum making_status: { 着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3, }
 
+
+
+
+
+  #enum making_status: { not_started: 0, pending: 1, in_progress: 2, completed: 3, }
 end
